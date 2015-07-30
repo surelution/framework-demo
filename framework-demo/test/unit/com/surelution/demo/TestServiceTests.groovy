894fa4510ef961882a1253d7986e8c7204e9d7c0
boyxgz <boyxgz@msn.com>
2015-07-30 15:36:32 +0800

    i

---
+++ framework-demo/test/unit/com/surelution/demo/TestServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.surelution.demo
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(TestService)
+class TestServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
